085b4e451f046f36421e746d08e4634d45ae36da
ezntek <eason@ezntek.com>
2024-08-22 17:03:08 +0800

    fix: code cleanup, run black

--- weather.py
+++ weather.py
@@ -1,35 +1,51 @@
+# weather: get the weather on the CLI
+#
+# Copyright (C) ja4e, Eason Qin, 2024.
+#
+# This source code form is licensed under the MIT/expat license. This program is provided
+# on an AS IS BASIS with NO implications or expression of warranty. Please visit the
+# OSI website for a full text of the license. 
+#
+
 import asyncio
 import python_weather
 import time
 import json
 import pycountry
 import os
+
 from rapidfuzz import process, fuzz
 
 user_entries = {}
 
-CACHE_FILE = 'weather_cache.json'
+CACHE_FILE = "weather_cache.json"
 CACHE_TIMEOUT = 2000
 
-country_dict = {country.name.upper(): country.alpha_2 for country in pycountry.countries}
+country_dict = {
+    country.name.upper(): country.alpha_2 for country in pycountry.countries
+}
+
 
 def load_cache():
     try:
-        with open(CACHE_FILE, 'r') as f:
+        with open(CACHE_FILE, "r") as f:
             data = json.load(f)
             # Remove expired cache entries
-            data['matches'] = {
-                loc: match for loc, match in data.get('matches', {}).items()
-                if time.time() - data['timestamp'] < CACHE_TIMEOUT
+            data["matches"] = {
+                loc: match
+                for loc, match in data.get("matches", {}).items()
+                if time.time() - data["timestamp"] < CACHE_TIMEOUT
             }
             return data
     except (FileNotFoundError, json.JSONDecodeError):
-        return {'timestamp': time.time(), 'matches': {}}
+        return {"timestamp": time.time(), "matches": {}}
+
 
 def save_cache(data):
-    data['timestamp'] = time.time()
-    with open(CACHE_FILE, 'w') as f:
+    data["timestamp"] = time.time()
+    with open(CACHE_FILE, "w") as f:
         json.dump(data, f, indent=4)
+
 
 def clear_cache():
     if os.path.exists(CACHE_FILE):
@@ -38,54 +54,104 @@ def clear_cache():
     else:
         print("Cache file does not exist.")
 
+
 def get_country_flag(country_code):
     try:
-        flag = ''.join([chr(0x1F1E6 + ord(c) - ord('A')) for c in country_code.upper()])
+        flag = "".join([chr(0x1F1E6 + ord(c) - ord("A")) for c in country_code.upper()])
         return flag
     except Exception as e:
         print(f"Error retrieving flag for {country_code}: {e}")
-        return ':question:'
+        return ":question:"
+
 
 def get_country_code(location_name):
     location_name = location_name.upper()
-    matches = process.extract(location_name, country_dict.keys(), scorer=fuzz.ratio, limit=15)
+    matches = process.extract(
+        location_name, country_dict.keys(), scorer=fuzz.ratio, limit=15
+    )
     exact_match = [match for match in matches if match[0] == location_name]
     if exact_match:
         matches = exact_match
     else:
         # Sort matches based on score (highest first)
         matches = sorted(matches, key=lambda x: x[1], reverse=True)
-    
+
     cache = load_cache()
-    cache['matches'][location_name] = matches
+    cache["matches"][location_name] = matches
     save_cache(cache)
     return matches
+
 
 def exit_program():
     print("Exiting the program.")
     exit()
 
-USER_ENTRIES_FILE = 'user_entries.json'
+
+USER_ENTRIES_FILE = "user_entries.json"
+
 
 def load_user_entries():
     try:
-        with open(USER_ENTRIES_FILE, 'r') as f:
+        with open(USER_ENTRIES_FILE, "r") as f:
             return json.load(f)
     except (FileNotFoundError, json.JSONDecodeError):
         return {}
 
+
 def save_user_entries():
-    with open(USER_ENTRIES_FILE, 'w') as f:
+    with open(USER_ENTRIES_FILE, "w") as f:
         json.dump(user_entries, f, indent=4)
+
 
 def clear_user_entries():
     global user_entries
+
     user_entries.clear()
     print("User entries have been cleared.")
 
+
+def get_location_choice(matches, location_name):
+    choice = int(input(f"Please select the correct option (1-{len(matches) + 5}): "))
+
+    choice -= len(matches)
+    match choice:
+        case 1:
+            manual_entry = input("Please enter the correct location manually: ").upper()
+            country = input(f"Enter the country for {manual_entry}: ").upper()
+            if (
+                input(
+                    f"Do you want to save {manual_entry} as {country}? (y/n): "
+                ).lower()
+                == "y"
+            ):
+                user_entries[manual_entry] = country
+            return manual_entry
+        case 2:
+            manual_entry = input("Please enter the correct location manually: ").upper()
+            return manual_entry
+        case 3:
+            clear_cache()
+            return prompt_for_location(location_name)
+        case 4:
+            clear_user_entries()
+            return prompt_for_location(location_name)
+        case 5:
+            clear_cache()
+            clear_user_entries()
+            return prompt_for_location(location_name)
+        case 6:
+            exit_program()
+        case _:
+            if 1 <= choice <= len(matches):
+                selected_location = matches[choice - 1][0]
+                return selected_location
+            else:
+                print("Invalid choice. Please try again.")
+
+
 def prompt_for_location(location_name):
     cache = load_cache()
-    matches = cache.get('matches', {}).get(location_name)
+    matches = cache.get("matches", {}).get(location_name)
 
     if not matches:
         matches = get_country_code(location_name)
@@ -111,91 +177,95 @@ def prompt_for_location(location_name):
     print(f"{len(matches) + 4}. Clear User Entries")
     print(f"{len(matches) + 5}. Clear All Cache")
     print(f"{len(matches) + 6}. Exit")
+
     while True:
         try:
-            choice = int(input(f"Please select the correct option (1-{len(matches) + 5}): "))
-            
-            if choice == len(matches) + 1:
-                manual_entry = input("Please enter the correct location manually: ").upper()
-                country = input(f"Enter the country for {manual_entry}: ").upper()
-                if input(f"Do you want to save {manual_entry} as {country}? (y/n): ").lower() == 'y':
-                    user_entries[manual_entry] = country
-                return manual_entry
-            elif choice == len(matches) + 2:
-                manual_entry = input("Please enter the correct location manually: ").upper()
-                return manual_entry
-            elif choice == len(matches) + 3:
-                clear_cache()
-                return prompt_for_location(location_name)
-            elif choice == len(matches) + 4:
-                clear_user_entries()
-                return prompt_for_location(location_name)
-            elif choice == len(matches) + 5:
-                clear_cache()
-                clear_user_entries()
-                return prompt_for_location(location_name)
-                return prompt_for_location(location_name)
-            elif choice == len(matches) + 6:
-                exit_program()
-            elif 1 <= choice <= len(matches):
-                selected_location = matches[choice - 1][0]
-                return selected_location
-            else:
-                print("Invalid choice. Please try again.")
+            get_location_choice(matches, location_name)
         except ValueError:
             print("Invalid input. Please enter a number.")
+
 
 def fahrenheit_to_celsius(fahrenheit):
     return (fahrenheit - 32) / 1.8
 
+
+def print_daily_forecast(daily):
+    high_temp_celsius = fahrenheit_to_celsius(daily.highest_temperature)
+    low_temp_celsius = fahrenheit_to_celsius(daily.lowest_temperature)
+
+    print(f"  Date: {daily.date}")
+
+    print(
+        f"  Highest Temperature: {daily.highest_temperature}°F ({high_temp_celsius:.1f}°C)"
+    )
+
+    print(
+        f"  Lowest Temperature: {daily.lowest_temperature}°F ({low_temp_celsius:.1f}°C)"
+    )
+
+    print(f"  Sunrise: {daily.sunrise}, Sunset: {daily.sunset}")
+    print(f"  Moon Phase: {daily.moon_phase.emoji} ({daily.moon_phase.name})")
+    print(f"  Snowfall: {daily.snowfall} inches")
+    print("  Hourly Forecast:")
+
+    for hourly in daily.hourly_forecasts:
+        time_formatted = hourly.time.strftime("%H:%M")
+        hourly_temp_celsius = fahrenheit_to_celsius(hourly.temperature)
+
+        print(
+            f"    → {time_formatted}: {hourly.temperature}°F ({hourly_temp_celsius:.1f}°C), {hourly.description}"
+        )
+
+        print(
+            f"      Wind Speed: {hourly.wind_speed} mph, {hourly.wind_direction.emoji} ({hourly.wind_direction.name})"
+        )
+
+        print(
+            f"      Humidity: {hourly.humidity}%, UV Index: {hourly.ultraviolet.index} ({hourly.ultraviolet.name})"
+        )
+    print("-" * 40)
+
+
+async def print_overview(location_name, weather):
+
+    current_temp_fahrenheit = weather.temperature
+    current_temp_celsius = fahrenheit_to_celsius(current_temp_fahrenheit)
+    country_code = country_dict.get(location_name.upper())
+    country_flag = (
+        get_country_flag(country_code) if country_code else "No flag available"
+    )
+
+    print(f"\n🌡️ Current Weather in {location_name}: {country_flag}")
+    print(f"  Temperature: {current_temp_fahrenheit}°F ({current_temp_celsius:.1f}°C)")
+    print(f"  Humidity: {weather.humidity}%")
+    print(
+        f"  Wind Speed: {weather.wind_speed} mph, {weather.wind_direction.emoji} ({weather.wind_direction.name})"
+    )
+    print(f"  UV Index: {weather.ultraviolet.index} ({weather.ultraviolet.name})\n")
+
+
 async def get_weather_forecast(location_name):
     async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
-        weather = await client.get(location_name)
-
-        current_temp_fahrenheit = weather.temperature
-        current_temp_celsius = fahrenheit_to_celsius(current_temp_fahrenheit)
-        country_code = country_dict.get(location_name.upper())
-        country_flag = get_country_flag(country_code) if country_code else 'No flag available'
-        
-        print(f"\n🌡️ Current Weather in {location_name}: {country_flag}")
-        print(f"  Temperature: {current_temp_fahrenheit}°F ({current_temp_celsius:.1f}°C)")
-        print(f"  Humidity: {weather.humidity}%")
-        print(f"  Wind Speed: {weather.wind_speed} mph, {weather.wind_direction.emoji} ({weather.wind_direction.name})")
-        print(f"  UV Index: {weather.ultraviolet.index} ({weather.ultraviolet.name})\n")
-
         print("📅 Daily Forecast:")
+
+        weather = await client.get(location_name)
+        await print_overview(location_name, weather)
+
         daily_forecasts = list(weather.daily_forecasts)
         for daily in daily_forecasts[:3]:  # Get the first 3 days of forecasts
-            high_temp_celsius = fahrenheit_to_celsius(daily.highest_temperature)
-            low_temp_celsius = fahrenheit_to_celsius(daily.lowest_temperature)
-            
-            print(f"  Date: {daily.date}")
-            print(f"  Highest Temperature: {daily.highest_temperature}°F ({high_temp_celsius:.1f}°C)")
-            print(f"  Lowest Temperature: {daily.lowest_temperature}°F ({low_temp_celsius:.1f}°C)")
-            print(f"  Sunrise: {daily.sunrise}, Sunset: {daily.sunset}")
-            print(f"  Moon Phase: {daily.moon_phase.emoji} ({daily.moon_phase.name})")
-            print(f"  Snowfall: {daily.snowfall} inches")
-            print("  Hourly Forecast:")
-            for hourly in daily.hourly_forecasts:
-                time_formatted = hourly.time.strftime("%H:%M")
-                hourly_temp_celsius = fahrenheit_to_celsius(hourly.temperature)
-                print(f"    → {time_formatted}: {hourly.temperature}°F ({hourly_temp_celsius:.1f}°C), {hourly.description}")
-                print(f"      Wind Speed: {hourly.wind_speed} mph, {hourly.wind_direction.emoji} ({hourly.wind_direction.name})")
-                print(f"      Humidity: {hourly.humidity}%, UV Index: {hourly.ultraviolet.index} ({hourly.ultraviolet.name})")
-            print("-" * 40)
+            print_daily_forecast(daily)
 
-async def get_weather(location):
-    await get_weather_forecast(location)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     user_entries.update(load_user_entries())
-    location = input("Please enter the location for which you want the weather forecast: ").upper()
+    location = input(
+        "Please enter the location for which you want the weather forecast: "
+    ).upper()
     location = prompt_for_location(location)
 
-    if os.name == 'nt':
+    if os.name == "nt":
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-    asyncio.run(get_weather(location))
+    asyncio.run(get_weather_forecast(location))
 
     save_user_entries()
-
